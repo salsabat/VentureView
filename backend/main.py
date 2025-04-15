1cb04e32cfc3de57b8a0580498ef652192b0c362
Sajal Sabat <sajalsabat@gmail.com>
2025-04-14 23:11:17 -0400

    Added/implemented forecast endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, UploadFile, File, Form, Depends
-import uvicorn
 from services.parser import parse_input
 from services.data_formats import ParseInput
+from services.uploader import save_csv_to_database, load_latest_user_upload
+from services.forecast import run_forecast
+from fastapi import FastAPI, UploadFile, File, Form, Depends
+import uvicorn
 from sqlalchemy.orm import Session
 from db import get_db
-from services.uploader import save_csv_to_database
 
 
 app = FastAPI(title="BDIS")
@@ -16,10 +17,20 @@ def parser(data: ParseInput):
 
 
 @app.post('/upload')
-def upload_csv(user_id: str = Form(...),
-               file: UploadFile = File(...),
-               db: Session = Depends(get_db)):
+def upload_csv(user_id: str = Form(...), file: UploadFile = File(...), db: Session = Depends(get_db)):
     return save_csv_to_database(user_id=user_id, file=file, db=db)
+
+
+@app.post('/forecast')
+def forecaster(user_id: str, product: str, horizon: int = 30, db: Session = Depends(get_db)):
+    try:
+        latest_df = load_latest_user_upload(user_id, db)
+        return run_forecast(product, latest_df, horizon)
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
 
 
 if __name__ == "__main__":
